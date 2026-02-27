efabc85753cc861681d770032753fcc8122191c4
JosephGuarino <108587385+JosephGuarino@users.noreply.github.com>
2026-02-26 20:49:55 -0600

    Add files via upload

---
+++ scripts/comm-box.js
@@ -0,0 +1,17 @@
+function toggleComm(box) {
+	var clickBox = document.getElementById(box);
+	var others = document.getElementsByClassName("comm-box");
+	for (var i = 0; i < others.length; i++) {
+		others[i].style.display = "none";
+	}
+	clickBox.style.display = "block";
+}
+
+function defaultOpen(defaultTab) {
+	toggleComm(defaultTab); // Show the default tab passed as a parameter
+}
+
+window.onload = function() {
+    var defaultTab = 'recent'; // Set the default tab ID
+    defaultOpen(defaultTab); // Call the function to open the default tab on load
+};
